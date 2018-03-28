225592ae63609f51ba8fc519bfb6b23e59273ef0
Anahita Vahdani <ana.vahdani@gmail.com>
2018-03-28 12:21:53 +0200

    modified bootstrap link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const html = `
   <head>
     <meta charset="utf-8">
     <title>Mario Kart Contest</title>
-    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
     <div id="main">
