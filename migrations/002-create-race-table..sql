460a7bf561f2fdf711915a57488fbcf604f80f90
Anahita <anahita.vahdani@neuf.fr>
2018-04-11 14:46:30 +0200

    createnew json and database files

---
+++ migrations/002-create-race-table..sql
@@ -0,0 +1,18 @@
+-- Up
+CREATE TABLE races (
+  id INTEGER PRIMARY KEY,
+  date TEXT
+);
+
+CREATE TABLE players_has_races (
+  race_id INTEGER,
+  player_id INTEGER,
+  position INTEGER
+  FOREIGN KEY(race_id) REFERENCES races(id)
+  FOREIGN KEY(player_id) REFERENCES members(id)
+);
+
+
+-- Down
+DROP TABLE players_has_races;
+DROP TABLE races;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const Promise = require('bluebird')
 const bodyParser = require('body-parser')
 const usersSeed = require('./public/members.json')
+const racesSeed = require('./public/races.json')
 const app = express()
 let db
 
@@ -15,6 +16,16 @@ const insertMember = m => {
   .then(() => db.get('SELECT last_insert_rowid() as id'))
   .then(({ id }) => db.get('SELECT * from members WHERE id = ?', id))
 }
+
+
+//date format YYYY-MM-DD HH:MM:SS.SSS
+const insertRace = r => {
+  const { date, } = r
+  return db.get('INSERT INTO races(date) VALUES(?)', date)
+  .then(() => db.get('SELECT last_insert_rowid() as id'))
+  .then(({ id }) => db.get('SELECT * from races WHERE id = ?', id))
+}
+
 // code qui remplit la db exemple
 const dbPromise = Promise.resolve()
 .then(() => sqlite.open('./database.sqlite', { Promise }))
@@ -23,6 +34,7 @@ const dbPromise = Promise.resolve()
   return db.migrate({ force: 'last' })
 })
 .then(() => Promise.map(usersSeed, m => insertMember(m)))
+.then(() => Promise.map(racesSeed, r => insertRace(r)))
 
 
 
