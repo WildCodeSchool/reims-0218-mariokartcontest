1e45b0c206f8d11f292a5397372e60015cb09448
Anthony <elmorodjeur@gmail.com>
2018-04-18 12:05:53 +0200

    second test succeed

--- public/players.js
+++ public/players.js
@@ -1,3 +1,11 @@
-const makePlayer = player => `<div>This is my list</div>`
+const makePlayer = player => `
+<div class="col-12 col-md-3">
+  <div class="card mb-4 box-shadow">
+  <img class="card-img-top" src="https://ih1.redbubble.net/image.52041676.7393/flat,800x800,070,f.u2.jpg" alt="avatar" />
+    <div class="card-body">
+      <p class="card-text">Erenude's profile</p>
+    </div>
+  </div>
+</div>`
 
-export default makePlayer
+export default makePlayer 
