cde431ebc1abd1b1f2f545a4158af5801d562efc
Anthony <elmorodjeur@gmail.com>
2018-04-18 14:24:33 +0200

    show members avatar

--- public/players.js
+++ public/players.js
@@ -1,9 +1,9 @@
 const makePlayer = player => `
 <div class="col-12 col-md-3">
   <div class="card mb-4 box-shadow">
-  <img class="card-img-top" src="https://ih1.redbubble.net/image.52041676.7393/flat,800x800,070,f.u2.jpg" alt="avatar" />
+  <img class="card-img-top" src=${player.image} alt="avatar" />
     <div class="card-body">
-      <p class="card-text">Erenude's profile</p>
+      <p class="card-text">${player.name}</p>
     </div>
   </div>
 </div>`
