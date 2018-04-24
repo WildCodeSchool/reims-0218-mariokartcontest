5c40ad5a5c14b044bf4d3ce5a257193149bbdded
Cynnah <massot.dorian@gmail.com>
2018-04-24 17:55:14 +0200

    bug fixed

--- passport-strategy.js
+++ passport-strategy.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
-
+const passportJWT = require("passport-jwt")
+const JWTStrategy   = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
 
 passport.use(new LocalStrategy({
         usernameField: 'username',
