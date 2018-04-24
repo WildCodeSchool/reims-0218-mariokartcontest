25b171f389455d3ec33fd95243c6d239aebf7f44
Cynnah <massot.dorian@gmail.com>
2018-04-24 15:05:49 +0200

    add login on passport-strategy file

--- passport-strategy.js
+++ passport-strategy.js
@@ -1,17 +1,24 @@
+const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
+
+// return an user if password is valid (coco)
+const getFakeUser = ({username, password}) => Promise.resolve(
+    password === 'coco' ? {username, password} : null
+)
+
 passport.use(new LocalStrategy({
         usernameField: 'username',
         passwordField: 'password'
     }, 
     function (username, password, cb) {
         //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-        return UserModel.findOne({username, password})
-           .then(user => {
-               if (!user) {
-                   return cb(null, false, {message: 'Incorrect username or password.'});
-               }
-               return cb(null, user, {message: 'Logged In Successfully'});
-          })
-          .catch(err => cb(err));
+        return 
+            getFakeUser.then(user => {
+                if (!user) {
+                    return cb(null, false, {message: 'Incorrect username or password.'});
+                }
+                return cb(null, user, {message: 'Logged In Successfully'});
+            })
+            .catch(err => cb(err));
     }
 ));
