dfa6ff7f0aa107bc73fb693be5892e665522a828
Cynnah <massot.dorian@gmail.com>
2018-04-25 16:27:41 +0200

    get token on admin section

--- public/app.js
+++ public/app.js
@@ -209,18 +209,46 @@ const controllers = {
           <div id="alert-box" class="hidden">
           </div>
           <h2>Log In</h2>
-          <form id="add-member">
+          <form id="logInForm">
             <div class="form-group">
               <label for="inputName">Username</label>
-              <input name="name" type="text" class="form-control" id="inputFirstName">
+              <input name="username" type="text" class="form-control" id="inputFirstName">
           </div>
           <div class="form-group">
             <label for="inputNickname">Password</label>
-            <input name="nickname" type="text" class="form-control" id="inputNickname">
+            <input name="password" type="text" class="form-control" id="inputNickname">
           </div>
           <button type="submit" class="btn btn-primary">Connexion</button>
-      `
-      )},
+          </form>
+      `)
+
+      const logInForm = document.getElementById('logInForm')
+      logInForm.addEventListener('submit', e => {
+        e.preventDefault()
+          //data
+          const data = serializeForm(logInForm)
+          console.log(data)
+
+          //POST sur le serveur /auth/login
+          fetch('/auth/login', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          })
+          .then(res => res.json())
+          .then(data => {
+            if(!data.user) {
+              //alert class danger
+            } else {
+              //store the token
+              localStorage.setItem('token', data.token)
+            }
+          });
+      });
+    },
     
     '/members/new': () => {
       //construit le formulaire
