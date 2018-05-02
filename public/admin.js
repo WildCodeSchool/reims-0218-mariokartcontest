4cb4c5ad3a5e55eb52516a632c2f6408c461dd06
Anahita <anahita.vahdani@neuf.fr>
2018-05-02 22:15:22 +0200

    add container

--- public/admin.js
+++ public/admin.js
@@ -5,22 +5,23 @@ import { serializeForm } from './utils.js'
 export const admin = 
 () => {
   render(`${navbar}
-  <div class="container mt-4">
-          <div id="alert-login"></div>
-          </div>
-          <h2>Log In</h2>
-          <form id="logInForm">
-            <div class="form-group">
-              <label for="inputName">Username</label>
-              <input name="username" type="text" class="form-control" id="inputFirstName">
-          </div>
-          <div class="form-group">
-            <label for="inputNickname">Password</label>
-            <input name="password" type="password" class="form-control" id="inputNickname">
-          </div>
-          <button type="submit" class="btn btn-primary">Connexion</button>
-          <button type="button" class="btn btn-primary" id="disconnect">Déconnexion</button>
-          </form>`)
+    <div class="container mt-4">
+      <div id="alert-login"></div>
+      <h2>Log In</h2>
+      <form id="logInForm">
+        <div class="form-group">
+          <label for="inputName">Username</label>
+          <input name="username" type="text" class="form-control" id="inputFirstName">
+        </div>
+        <div class="form-group">
+          <label for="inputNickname">Password</label>
+          <input name="password" type="password" class="form-control" id="inputNickname">
+        </div>
+      <button type="submit" class="btn btn-primary">Connexion</button>
+      <button type="button" class="btn btn-primary" id="disconnect">Déconnexion</button>
+      </form>
+    </div>
+  `)
 
   const logInForm = document.getElementById('logInForm')
   logInForm.addEventListener('submit', e => {
